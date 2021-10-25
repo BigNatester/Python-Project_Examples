ec51cb38a9893a2e2a3f07bfa185c7a451a7092a
Nathan Mills <91986937+BigNatester@users.noreply.github.com>
2021-10-24 19:01:44 -0700

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,6 +1,6 @@
 import time #Allows interaction witht the time object
 import sys #Allows for interaction with the system object. 
-from playsound import playsound #Will play the sound when the number reaches 0. install using pip install playsound in your command prompt on windows or pip3 install playsound
+from playsound import playsound #Will play the sound when the number reaches 0. install using pip install playsound in your command prompt on windows or pip3 install playsound on linux
 #Download and place it into a folder for the script to use 
 
 x = 10 #Define number to count down from 
